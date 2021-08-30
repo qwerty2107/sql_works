766ba2e984cdc2362b26493065ba2832f2ec74bc
qwerty2107 <86987204+qwerty2107@users.noreply.github.com>
2021-08-30 13:33:58 +0300

    _

---
+++ Query_scripts/Sales/query4.py
@@ -0,0 +1,15 @@
+import mysql.connector
+database_name = 'example'
+example = mysql.connector.connect(user='root', password='password',
+                              host='127.0.0.1',
+                              database= database_name)
+cursor = example.cursor()
+cursor.execute(('select orders.id, orders.date, sum(quantity*price) from orders ' +
+'left join order_items on orders.id = order_items.order_id ' +
+'left join goods on order_items.goods_id = goods.id ' +
+'group by orders.id, orders.date'))
+for n in cursor:
+    print(n)
+cursor.close()
+example.commit()
+example.close()
