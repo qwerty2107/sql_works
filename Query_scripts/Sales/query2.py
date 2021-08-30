d2ec93944ccb9aadca06544fb783aec36f803b28
qwerty2107 <86987204+qwerty2107@users.noreply.github.com>
2021-08-30 12:24:37 +0300

    _

---
+++ Query_scripts/Sales/query2.py
@@ -0,0 +1,15 @@
+import mysql.connector
+database_name = 'example'
+example = mysql.connector.connect(user='root', password='password',
+                              host='127.0.0.1',
+                              database= database_name)
+cursor = example.cursor()
+cursor.execute(('select avg(price*quantity) from order_items' +
+'left join (orders, goods)' +
+'on (orders.id = order_items.order_id' +
+'and goods.id = order_items.goods_id')))
+for n in cursor:
+    print(n)
+cursor.close()
+example.commit()
+example.close()
---
+++ Query_scripts/Sales/update2.py
@@ -0,0 +1,24 @@
+import mysql.connector
+database_name = 'example'
+example = mysql.connector.connect(user='root', password='password',
+                              host='127.0.0.1',
+                              database= database_name)
+cursor = example.cursor()
+cursor.execute(('update goods' +
+'set price = price + 100' +
+'where id not in' +
+'(' +
+  'select goods_id from order_items' +
+  'where order_id in' +
+  '(' +
+    'select id from orders' +
+    'where month(date) = 07' +
+  ')' +
+')'
+))
+
+for n in cursor:
+    print(n)
+cursor.close()
+example.commit()
+example.close()
