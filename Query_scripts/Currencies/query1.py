df50164a55d35d3cd98e1785da5de52911c98f70
qwerty2107 <86987204+qwerty2107@users.noreply.github.com>
2021-09-01 15:41:50 +0300

    _

--- Query_scripts/Currencies/query1.py
+++ Query_scripts/Currencies/query1.py
@@ -4,15 +4,17 @@ example = mysql.connector.connect(user='root', password='password',
                               host='127.0.0.1',
                               database= database_name)
 cursor = example.cursor()
+currency_code = 'ABC'
+date = '2021-07-28'
 cursor.execute(('select currencies.code, ratios.date, ratios.value from currencies'\
 'right join ratios'\
 'on currencies.id = ratios.currency_id'\
-"where currencies.code = 'ABC' and ratios.date in"\
+f"where currencies.code = 'ABC' and ratios.date in"\
 '('\
   'select max(date) from currencies'\
   'right join ratios'\
   'on currencies.id = ratios.currency_id'\
-  "where currencies.code = 'ABC' and ratios.date<='2021-07-28'"\
+  f"where currencies.code = {currency_code} and ratios.date<={date}"\
 ')'))
 for n in cursor:
     print(n)
--- Query_scripts/Sales/query1.py
+++ Query_scripts/Sales/query1.py
@@ -4,10 +4,11 @@ example = mysql.connector.connect(user='root', password='password',
                               host='127.0.0.1',
                               database= database_name)
 cursor = example.cursor()
+month = 7
 cursor.execute(('select sum(quantity) from order_items '\
 'left join orders '\
 'on orders.id = order_items.order_id '\
-'where month(orders.date) = 7 '))
+f'where month(orders.date) = {month} '))
 for n in cursor:
     print(n)
 cursor.close()
